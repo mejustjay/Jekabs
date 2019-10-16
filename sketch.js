5c9fbe2004229af259eea6acfe2c8111bbc48ef8
mejustjay <jekabskarlisknets@gmail.com>
2019-10-16 10:32:38 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,12 @@
 var canvas;
-var mic;
 
 function windowResized() {
-  //console.log('resized');
   resizeCanvas(windowWidth, windowHeight);
 }
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
-  // canvas.style('z-index', '-1');
-  // background(175);
 }
 
 function keyPressed() {
