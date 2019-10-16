ef9d8db2ca9dd103875fde01d69ebe4d3d71d92f
mejustjay <jekabskarlisknets@gmail.com>
2019-10-16 10:17:58 +0300

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,11 +1,28 @@
+var canvas;
+var mic;
 
-function setup () {
-  createCanvas (500, 500);
+function windowResized() {
+  //console.log('resized');
+  resizeCanvas(windowWidth, windowHeight);
+}
 
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  //canvas.style('z-index', '-1');
+  // mic = new p5.AudioIn();
+  // mic.start();
+  //background(175);
+}
 
+function keyPressed() {
+  clear();
 }
 
 function draw() {
-  background(0);
-  ellipse(50, 50, 80, 80);
+  if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+  // var vol = mic.getLevel();
+  // ellipse(width / 2, height / 2, vol * width);
 }
