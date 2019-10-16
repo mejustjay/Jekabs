966064a18b3c36ee965cdbd68ea99926bb0de1ef
mejustjay <jekabskarlisknets@gmail.com>
2019-10-16 09:29:35 +0300

    Create background.js

---
+++ background.js
@@ -0,0 +1,16 @@
+var canvas;
+
+
+function setup () {
+  canvas = createCanvas (windowWidth, windowHeight);
+  canvas.position (0,0);
+  canvas.style ('z-index', '-1');
+
+}
+
+function draw() {
+  background(110);
+
+
+
+}
