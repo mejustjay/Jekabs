2cf005e3257c2b89c46e6081163fd29f5fddc52d
mejustjay <jekabskarlisknets@gmail.com>
2019-10-16 16:52:51 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,14 @@
 var canvas;
 
+let rad = 60; // Width of the shape
+let xpos, ypos; // Starting position of shape
+
+let xspeed = 2.8; // Speed of the shape
+let yspeed = 2.2; // Speed of the shape
+
+let xdirection = 1; // Left or Right
+let ydirection = 1; // Top to Bottom
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
@@ -7,6 +16,16 @@ function windowResized() {
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
+  canvas.style('z-index','-1');
+  background (110,200,10);
+
+
+  noStroke();
+  frameRate(30);
+  ellipseMode(RADIUS);
+  // Set the starting position of the shape
+  xpos = width / 2;
+  ypos = height / 2;
 }
 
 function keyPressed() {
@@ -17,4 +36,18 @@ function draw() {
   if (mouseIsPressed) {
     line(pmouseX, pmouseY, mouseX, mouseY);
   }
+  xpos = xpos + xspeed * xdirection;
+  ypos = ypos + yspeed * ydirection;
+
+  // Test to see if the shape exceeds the boundaries of the screen
+  // If it does, reverse its direction by multiplying by -1
+  if (xpos > width - rad || xpos < rad) {
+    xdirection *= -1;
+  }
+  if (ypos > height - rad || ypos < rad) {
+    ydirection *= -1;
+  }
+
+  // Draw the shape
+  ellipse(xpos, ypos, rad, rad);
 }
