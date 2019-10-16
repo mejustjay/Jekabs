878bf466d5121cc779432f197e4d2b4eb69a858d
mejustjay <jekabskarlisknets@gmail.com>
2019-10-16 10:26:31 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,10 +9,8 @@ function windowResized() {
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
-  //canvas.style('z-index', '-1');
-  // mic = new p5.AudioIn();
-  // mic.start();
-  //background(175);
+  // canvas.style('z-index', '-1');
+  // background(175);
 }
 
 function keyPressed() {
@@ -24,5 +22,4 @@ function draw() {
     line(pmouseX, pmouseY, mouseX, mouseY);
   }
   // var vol = mic.getLevel();
-  // ellipse(width / 2, height / 2, vol * width);
 }
